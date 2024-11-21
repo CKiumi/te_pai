e353f0cd4b918e5a58316f071a829c2dc532976a
CKiumi <c.kiumi.oval17@gmail.com>
2024-11-21 14:34:11 +0900

    detach trotter class

--- examples/main.py
+++ examples/main.py
@@ -1,5 +1,6 @@
 from te_pai.hamil import Hamiltonian
 from te_pai.trotter import Trotter
+from te_pai.te_pai import TE_PAI
 from te_pai.sampling import resample
 import numpy as np
 
@@ -10,22 +11,27 @@ if __name__ == "__main__":
     T = 1  # Total evolution time
     N = 2000  # Number of Trotter steps
     n_snapshot = 10  # Number of snapshots
-    freqs = np.random.uniform(-1, 1, size=numQs)  # Random frequencies for spin chain
-
+    rng = np.random.default_rng(0)
+    freqs = rng.uniform(-1, 1, size=numQs)
     # Initialize Hamiltonian and Trotter simulation
     # Assuming a spin chain Hamiltonian constructor
     hamil = Hamiltonian.spin_chain_hamil(numQs, freqs)
-    trotter = Trotter(hamil, numQs, Δ, T, N, n_snapshot)
+    te_pai = TE_PAI(hamil, numQs, Δ, T, N, n_snapshot)
 
     # Print expected number of gates and overhead
-    print("Expected number of gates:", trotter.expected_num_gates)
-    print("Measurement overhead:", trotter.overhead)
+    print("Expected number of gates:", te_pai.expected_num_gates)
+    print("Measurement overhead:", te_pai.overhead)
 
-    # Run the Trotter simulation and resample the results
-    res = [resample(data) for data in trotter.run_te_pai(100)]
-
+    # Run the TE-PAI simulation and resample the results
+    res = [resample(data) for data in te_pai.run_te_pai(10000)]
     # Compute mean and standard deviation for the resampled data
     mean, std = zip(*[(np.mean(y), np.std(y)) for y in res])
 
-    print("Means of resampled data:", mean)
-    print("Standard deviations of resampled data:", std)
+    print("Means of TE-PAI result:", mean)
+    print("Standard deviations of TE-PAI result:", std)
+
+    # Use Lie Trotter to run the simulation
+    trotter = Trotter(hamil, numQs, T, N, n_snapshot)
+    res = [2 * prob - 1 for prob in trotter.lie_trotter()]
+    mean, std = zip(*[(np.mean(y), np.std(y)) for y in res])
+    print("Means of Trotter result:", mean)
--- te_pai/trotter.py
+++ te_pai/trotter.py
@@ -13,29 +13,11 @@ import pandas as pd
 @dataclass
 class Trotter:
 
-    def __init__(self, hamil, numQs, Δ, T, N, n_snap):
-        (self.nq, self.n_snap, self.Δ, self.T, self.N) = (numQs, n_snap, Δ, T, N)
+    def __init__(self, hamil, numQs, T, N, n_snap):
+        (self.nq, self.n_snap, self.T, self.N) = (numQs, n_snap, T, N)
         self.L = len(hamil)
         steps = np.linspace(0, T, N)
-        angles = [[2 * np.abs(coef) * T / N for coef in hamil.coefs(t)] for t in steps]
-        n = int(N / n_snap)
-        self.gam_list = [1] + [
-            np.prod([pai.gamma(angles[j], self.Δ) for j in range((i + 1) * n)])
-            for i in range(n_snap)
-        ]
-        self.gamma = self.gam_list[-1] if N > 0 else 0
-        self.probs = [pai.prob_list(angles[i], Δ) for i in range(N)]
         self.terms = [hamil.get_term(t) for t in steps]
-        self.overhead = np.exp(2 * hamil.l1_norm(T) * np.tan(Δ / 2))
-        self.expected_num_gates = ((3 - np.cos(Δ)) / np.sin(Δ)) * hamil.l1_norm(T)
-        self.rea_expect_num_gates = 0
-        for prob in self.probs:
-            for p in prob:
-                self.rea_expect_num_gates += 1 - p[0]
-
-    def sample_num_gates(self, n):
-        res = sampling.batch_sampling(np.array(self.probs), n)
-        return [sum(len(r) for r in re) for re in res]
 
     def get_lie_PDF(self, points=1000, exact=True):
         sn = 1000
@@ -44,18 +26,17 @@ class Trotter:
         data = [[2 * x / sn - 1, sn / 2 * val] for x, val in zip(x, pdf)]
         return zip(*data)
 
-    def lie_trotter(self, exact, err=None):
-        N = self.N if exact else int(np.ceil(self.expected_num_gates / self.L))
+    def lie_trotter(self, err=None):
         noisy = "_noisy" if err is not None else ""
-        filename = f"data/lie/lie{N}_snap{noisy}_step{self.n_snap}.csv"
+        filename = f"data/lie/lie{self.N}_snap{noisy}_step{self.n_snap}.csv"
         gates_arr = []
         if not os.path.exists(filename):
-            n = int(N / self.n_snap)
-            for i in range(N):
+            n = int(self.N / self.n_snap)
+            for i in range(self.N):
                 if i % n == 0:
                     gates_arr.append([])
                 gates_arr[-1] += [
-                    (pauli, 2 * coef * self.T / N, ind)
+                    (pauli, 2 * coef * self.T / self.N, ind)
                     for (pauli, ind, coef) in self.terms[i]
                 ]
             res = simulator.get_probs(self.nq, gates_arr, self.n_snap, err=err)
@@ -64,37 +45,3 @@ class Trotter:
         else:
             data = pd.read_csv(filename).values
             return data[:, 0]
-
-    # Main Algorithm for TE_PAI
-    def run_te_pai(self, num_circuits, err=None):
-        noisy = "_noisy" if err is not None else ""
-        filename = lambda i: f"data/pai_snap{noisy}{str(i)}.csv"
-        if not os.path.exists(filename(0)):
-            res = []
-            index = sampling.batch_sampling(np.array(self.probs), num_circuits)
-            res += mp.Pool(mp.cpu_count()).map(
-                partial(self.gen_rand_cir, err=err), index
-            )
-            res = np.array(res).transpose(1, 0, 2)
-            for i in range(self.n_snap + 1):
-                pd.DataFrame(res[i]).to_csv(filename(i), index=False)
-            return res
-        else:
-            return [pd.read_csv(filename(i)).values for i in range(self.n_snap + 1)]
-
-    def gen_rand_cir(self, index, err=None):
-        (gates_arr, sign, sign_list, n) = ([], 1, [], int(self.N / self.n_snap))
-        for i, inde in enumerate(index):
-            if i % n == 0:
-                gates_arr.append([])
-                sign_list.append(sign)
-            for j, val in inde:
-                (pauli, ind, coef) = self.terms[i][j]
-                if val == 3:
-                    sign *= -1
-                    gates_arr[-1].append((pauli, np.pi, ind))
-                else:
-                    gates_arr[-1].append((pauli, np.sign(coef) * self.Δ, ind))
-        sign_list.append(sign)
-        data = simulator.get_probs(self.nq, gates_arr, self.n_snap, err)
-        return np.array([(sign_list[i] * self.gam_list[i], data[i]) for i in range(self.n_snap + 1)])  # type: ignore
