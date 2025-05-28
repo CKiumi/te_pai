3096794ef8020267eee2d37ef57e110cb566811b
CKiumi <c.kiumi.oval17@gmail.com>
2025-05-29 07:39:52 +0900

    fix test

--- test/test_hamil.py
+++ test/test_hamil.py
@@ -6,5 +6,5 @@ from te_pai.hamil import Hamiltonian
 def test_spin_chain_hamil_structure():
     nqubits = 4
     freqs = np.ones(nqubits)
-    hamil = Hamiltonian.spin_chain_hamil(nqubits, freqs)
+    hamil = Hamiltonian.spin_chain_hamil(nqubits, freqs, 20)
     assert len(hamil.terms) == 16
